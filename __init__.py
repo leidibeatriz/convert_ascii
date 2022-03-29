4352141eef18e784aee58548b62a0ba95c08a2f8
leidibeatriz <94936204+leidibeatriz@users.noreply.github.com>
2022-03-29 11:02:27 -0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from convert_ascii import converte_para_decimal
-from convert_ascii import converte_para_letra
-
-if(__name__=="__main__"):
-
-    decimal = converte_para_decimal('d')
-    print("Valor: ",decimal)
-    numero= converte_para_letra('100')
-    print("Letra:",numero)
